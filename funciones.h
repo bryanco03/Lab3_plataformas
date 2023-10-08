b13de8b3d7bbc6313712e1f4bb4ebf2bcd381a45
bryanco03 <bryanco0310@gmail.com>
2023-10-08 17:14:51 -0600

    Header file para las funciones de maximos y minimos del ejecicio 2

---
+++ funciones.h
@@ -0,0 +1,9 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+int encontrarMinimo(int arreglo[], int longitud);
+int encontrarMaximo(int arreglo[], int longitud);
+void encontrarMinMax(int *arreglo, int *minimo, int *maximo, int longitud);
+
+#endif
+
